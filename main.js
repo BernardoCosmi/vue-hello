65340e53b6d4ca83cd2234f103f196bf0e49bdae
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-25 15:53:10 +0100

    added Vue script

---
+++ main.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue 
+
+createApp({
+    data() {
+        return{
+            messaggio: 'Non so bene cosa scriverci',
+
+            immagine: 'https://picsum.photos/500/400',
+        };
+    },
+}).mount('#app')
+
+// Esercizio di oggi: Vue Hello
+// nome repo: vue-hello
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
